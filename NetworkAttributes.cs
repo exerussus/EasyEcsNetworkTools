2dc0097157b91fd718089514ed5aa42df0152c48
exerussus <solovyov.production@gmail.com>
2025-03-16 16:16:48 +0300

    Auto updated to version 1.0.13

---
+++ NetworkAttributes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exerussus.EasyEcsNetworkTools
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class ClientMethodAttribute : Attribute
+    {
+        
+    }
+    
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class ServerMethodAttribute : Attribute
+    {
+        
+    }
+}
