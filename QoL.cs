9af54ad7e6f35b74fc85f37ed6864eb22c8b2a47
exerussus <solovyov.production@gmail.com>
2025-05-30 00:45:39 +0300

    Auto updated to version 1.0.23

--- QoL.cs
+++ QoL.cs
@@ -35,5 +35,37 @@ namespace Exerussus.EasyEcsNetworkTools
             pool.Del(ecsUniqEntity.EcsEntityId);
             return true;
         }
+        
+        public static ref T Add<T>(this PoolerModuleCustom<T> pool, EcsUniqEntity ecsUniqEntity) where T : struct, IEcsComponent
+        {
+            return ref pool.Add(ecsUniqEntity.EcsEntityId);
+        }
+        
+        public static ref T AddOrGet<T>(this PoolerModuleCustom<T> pool, EcsUniqEntity ecsUniqEntity) where T : struct, IEcsComponent
+        {
+            return ref pool.AddOrGet(ecsUniqEntity.EcsEntityId);
+        }
+        
+        public static ref T Get<T>(this PoolerModuleCustom<T> pool, EcsUniqEntity ecsUniqEntity) where T : struct, IEcsComponent
+        {
+            return ref pool.Get(ecsUniqEntity.EcsEntityId);
+        }
+        
+        public static bool Has<T>(this PoolerModuleCustom<T> pool, EcsUniqEntity ecsUniqEntity) where T : struct, IEcsComponent
+        {
+            return pool.Has(ecsUniqEntity.EcsEntityId);
+        }
+        
+        public static void Del<T>(this PoolerModuleCustom<T> pool, EcsUniqEntity ecsUniqEntity) where T : struct, IEcsComponent
+        {
+            pool.Del(ecsUniqEntity.EcsEntityId);
+        }
+        
+        public static bool TryDel<T>(this PoolerModuleCustom<T> pool, EcsUniqEntity ecsUniqEntity) where T : struct, IEcsComponent
+        {
+            if (!pool.Has(ecsUniqEntity.EcsEntityId)) return false;
+            pool.Del(ecsUniqEntity.EcsEntityId);
+            return true;
+        }
     }
 }
